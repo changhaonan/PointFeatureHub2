9e83f54d222ffe8595fe05366cea4e2435bbe962
changhaonan <chnme40cs@gmail.com>
2023-03-01 17:06:46 -0500

    update: further tuned the parameter

--- test/test_matcher32d_client.cpp
+++ test/test_matcher32d_client.cpp
@@ -14,25 +14,31 @@ int main(int argc, char **argv)
     matcher_client.SetUp();
 
     std::string sparse_model_path = (current_path / "data/3d_train/001").string();
-    cv::Mat image = cv::imread(image_path_list[0].string());
     std::vector<cv::KeyPoint> kpts2d;
     std::vector<Eigen::Vector3f> kpts3d;
-    kpts2d.clear();
-    kpts3d.clear();
     Eigen::Matrix3f K;
     K << 5.986332104667836802e+02, 0.000000000000000000e+00, 3.245491486036403330e+02,
         0.000000000000000000e+00, 5.901477555297449271e+02, 2.237197338492138670e+02,
         0.000000000000000000e+00, 0.000000000000000000e+00, 1.000000000000000000e+00;
-    matcher_client.Match32D(sparse_model_path, image, Eigen::Vector4f(0, image.cols * 0.5, 0, image.rows * 0.5), 0, K, kpts2d, kpts3d);
 
-    // Draw matches
-    cv::Mat image_with_matches;
-    // Use a full DMat to draw all matches
-    std::vector<cv::DMatch> matches;
-    for (auto i = 0; i < kpts2d.size(); i++)
+    for (auto &image_path : image_path_list)
     {
-        cv::DMatch match(i, i, 0);
-        matches.push_back(match);
+        cv::Mat image = cv::imread(image_path.string());
+        kpts2d.clear();
+        kpts3d.clear();
+
+        matcher_client.Match32D(sparse_model_path, image, Eigen::Vector4f(0, image.cols * 0.5, 0, image.rows * 0.5), 0, K, kpts2d, kpts3d);
+
+        // Draw matches
+        cv::Mat image_with_matches;
+        // Use a full DMat to draw all matches
+        std::vector<cv::DMatch> matches;
+        for (auto i = 0; i < kpts2d.size(); i++)
+        {
+            cv::DMatch match(i, i, 0);
+            matches.push_back(match);
+        }
     }
+
     return 0;
 }
